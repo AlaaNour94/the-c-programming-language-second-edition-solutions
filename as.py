cfd1defcbaf7220ebde9e8748d154d6b658e048d
Alaa <alaanourali@gmail.com>
2024-02-21 13:23:38 +0200

    .

---
+++ as.py
@@ -0,0 +1,13 @@
+pointer to function
+qsort in c
+uthash
+array of arrays  (char *, char ** , char ***)  how to deal with them, the size of them, how to malloc them
+
+
+
+
+
+
+
+
+
---
+++ play.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+int main(int argc, char** argv)
+{
+    printf("%*d\n", 7)    ;
+}
